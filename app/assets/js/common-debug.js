0b1a45ac769b223cf4ae843368a8e558dde95567
yt94dev <yarikT94@gmail.com>
2017-03-16 17:10:47 +0200

    added pricing page with mailer

--- app/assets/js/common-debug.js
+++ app/assets/js/common-debug.js
@@ -1,8 +1,164 @@
 $(document).ready(function(){
-  $('.singleslider').slick({
-    dots:true,
-    // autoplay: true
+  if ($('#singleslider-index').length > 0 ) {
+    $('.singleslider').slick({
+      dots:true,
+      // autoplay: true
+    });
+  }
+
+  //Code for small business modal
+
+  $('button#go-small').click( function(event){
+    event.preventDefault();
+    $('#overlay').fadeIn(400,
+      function(){
+        $('#modal_form-small')
+          .css('display', 'block')
+          .animate({opacity: 1, top: '50%'}, 200);
+      });
   });
+
+    $('.modal_close, #overlay').click( function(){
+      $('#modal_form-small')
+        .animate({opacity: 0, top: '45%'}, 200,
+          function(){
+            $(this).css('display', 'none');
+            $('#overlay').fadeOut(400);
+          }
+        );
+    });
+
+  //Code for small business modal
+
+
+  //Code for big business modal
+
+  $('button#go-big').click( function(event){
+    event.preventDefault();
+    $('#overlay').fadeIn(400,
+      function(){
+        $('#modal_form-big')
+          .css('display', 'block')
+          .animate({opacity: 1, top: '50%'}, 200);
+      });
+  });
+
+  $('.modal_close, #overlay').click( function(){
+    $('#modal_form-big')
+      .animate({opacity: 0, top: '45%'}, 200,
+        function(){
+          $(this).css('display', 'none');
+          $('#overlay').fadeOut(400);
+        }
+      );
+  });
+
+  //Code for big business modal
+
+
+
+  //Code for enterprise business modal
+
+  $('button#go-enterprise').click( function(event){
+    event.preventDefault();
+    $('#overlay').fadeIn(400,
+      function(){
+        $('#modal_form-enterprise')
+          .css('display', 'block')
+          .animate({opacity: 1, top: '50%'}, 200);
+      });
+  });
+
+  $('.modal_close, #overlay').click( function(){
+    $('#modal_form-enterprise')
+      .animate({opacity: 0, top: '45%'}, 200,
+        function(){
+          $(this).css('display', 'none');
+          $('#overlay').fadeOut(400);
+        }
+      );
+  });
+
+  //Code for enterprise business modal
+
+
+  // Small business modal form handler
+
+    $("#small-plan-form").submit(function(e) {
+      e.preventDefault();
+
+      $.ajax({
+        type: "POST",
+        url: "pricing-action.php",
+        data: $("#small-plan-form").serialize(),
+        dataType: 'text',
+        success: function(response) {
+          response = eval(response);
+          alert("Information is sent!");
+          $('#modal_form-small')
+            .animate({opacity: 0, top: '45%'}, 200,
+              function(){
+                $(this).css('display', 'none');
+                $('#overlay').fadeOut(400);
+              }
+            );
+        }
+      });
+    });
+
+
+  // Big business modal form handler
+
+  $("#big-plan-form").submit(function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: "POST",
+      url: "pricing-action.php",
+      data: $("#big-plan-form").serialize(),
+      dataType: 'text',
+      success: function(response) {
+        response = eval(response);
+        alert("Information is sent!");
+        $('#modal_form-big')
+          .animate({opacity: 0, top: '45%'}, 200,
+            function(){
+              $(this).css('display', 'none');
+              $('#overlay').fadeOut(400);
+            }
+          );
+      }
+    });
+  });
+
+
+
+  // Enterprise business modal form handler
+
+  $("#enterprise-plan-form").submit(function(e) {
+    e.preventDefault();
+
+    $.ajax({
+      type: "POST",
+      url: "pricing-action.php",
+      data: $("#enterprise-plan-form").serialize(),
+      dataType: 'text',
+      success: function(response) {
+        response = eval(response);
+        alert("Information is sent!");
+        $('#modal_form-enterprise')
+          .animate({opacity: 0, top: '45%'}, 200,
+            function(){
+              $(this).css('display', 'none');
+              $('#overlay').fadeOut(400);
+            }
+          );
+      }
+    });
+  });
+
+
+  
 
 });
 
---
+++ app/pricing-action.php
@@ -0,0 +1,57 @@
+<?
+
+if ((isset($_POST['username']) && $_POST['username'] != "")
+    && (isset($_POST['email']) && $_POST['email'] != "")
+    && (isset($_POST['phoneNum']) && $_POST['phoneNum'] != "")
+    && (isset($_POST['businessName']) && $_POST['businessName'] != "")
+    && (isset($_POST['businessType']) && $_POST['businessType'] != "")
+    && (isset($_POST['emplNumber']) && $_POST['emplNumber'] != "")
+    && (isset($_POST['pricePlanType']))
+
+) {
+    $features = '';
+    if (empty($_POST['featuresPlanSet'])) {
+        $features = "When submitting a form  ".$_POST['username']." deselected all features.";
+    } else {
+        $features = implode(', ', $_POST['featuresPlanSet']);
+    }
+
+    $username = $_POST['username'];
+    $username = ucfirst($username);
+
+    $businessName = $_POST['businessName'];
+    $businessName = strtoupper($businessName);
+
+    $businessType = $_POST['businessType'];
+    $businessType = strtoupper($businessType);
+
+    $pricePlanType = $_POST['pricePlanType'];
+    $pricePlanType = strtoupper($pricePlanType);
+
+    $to = 'yarikT94@gmail.com';
+    $subject = 'New subscribe for ' . $_POST['pricePlanType'] . ' pricing plan'; //Загаловок сообщения
+    $message = '
+                      <html>
+                          <head>
+                              <title>' . $subject . '</title>
+                          </head>
+                          <body>
+                              <p>Name: ' . $username . '</p>
+                              <p>E-mail: ' . $_POST['email'] . '</p>
+                              <p>Phone: ' . $_POST['phoneNum'] . '</p>
+                              <p>Business name: ' . $businessName . '</p>
+                              <p>Business type: ' . $businessType . '</p>
+                              <p>Number of Employees: ' . $_POST['emplNumber'] . '</p>
+                              <p>Price plan type: ' . $pricePlanType . '</p>
+                              <p>Pricing plan features: ' . $features .'</p>                                                        
+                          </body>
+                      </html>';
+
+
+    $headers = "Content-type: text/html; charset=utf-8 \r\n";
+    $headers .= "From: J.A. Pricing <from@jasonarsenault-pricing>\r\n";
+    mail($to, $subject, $message, $headers);
+
+}
+
+?>
